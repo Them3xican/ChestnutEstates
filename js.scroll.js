63f74f8aa3655ff1516f83451469d4143dc85975
George Houghton <georgehoughton24@live.co.uk>
2017-01-31 12:27:23 +0000

    fgnfn

---
+++ js.scroll.js
@@ -0,0 +1,11 @@
+var wrap = $("#container");
+
+wrap.on("scroll", function(e) {
+    
+  if (this.scrollTop > 147) {
+    wrap.addClass("fix-search");
+  } else {
+    wrap.removeClass("fix-search");
+  }
+  
+});
